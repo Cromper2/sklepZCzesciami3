865ecbc8c859865977f90063f629babf3e710e09
Karolina134 <117594414+Karolina134@users.noreply.github.com>
2022-11-13 17:48:11 +0100

    cdcssdc

--- src/main/java/pl/slowacki/sklepZCzesciami3/D/controller/Controller1.java
+++ src/main/java/pl/slowacki/sklepZCzesciami3/D/controller/Controller1.java
@@ -3,6 +3,10 @@ package pl.slowacki.sklepZCzesciami3.D.controller;
 import org.springframework.web.bind.annotation.*;
 import pl.slowacki.sklepZCzesciami3.D.controller.*;
 
+@RestController
 public class Controller1 {
-    @
+    @GetMapping("/hello")
+    public String hello(){
+        return "<h1 style = color: green; font-size: 50px;'>Hello, welcome in our computer shop!</h1>";
+    }
 }
